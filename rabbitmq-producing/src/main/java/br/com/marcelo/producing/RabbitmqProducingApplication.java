3f0688c669fed8aa0fdab62d25f5af248905da98
marcelowender <marcelowender@hotmail.com>
2021-07-25 01:07:12 -0300

    Creation of project consumer

---
+++ rabbitmq-producing/src/main/java/br/com/marcelo/producing/RabbitmqProducingApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcelo.producing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqProducingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqProducingApplication.class, args);
+	}
+
+}
---
+++ rabbitmq-producing/src/test/java/br/com/marcelo/producing/RabbitmqProducingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelo.producing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqProducingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
